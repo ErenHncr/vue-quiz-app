cdfb56825c5e8fc065bdc08ab23f3ffad8e4efb1
erenhncr <erenhncr@gmail.com>
2021-06-12 20:09:56 +0300

    vue.config updated

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 // vue.config.js
 module.exports = {
+  // this line added for fixing blank page in index.html
+  publicPath: process.env.NODE_ENV === 'production'
+    ? './'
+    : '/',
   css: {
     loaderOptions: {     
       scss: {
