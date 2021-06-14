f0567acc01003ce6dd03a2be5ee1c1c11312e88c
erenhncr <erenhncr@gmail.com>
2021-06-15 01:52:09 +0300

    Question number added

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -5,17 +5,16 @@
       v-for="(question, qIndex) in questions" 
       :key="question.q"
       v-show="qIndex === currentQuestion"
-    >
-      <div class="question" v-html="question.q" />
+    >     
+      <div class="question" v-html="questionWithNumber(qIndex, question.q)" />
       <div class="answers">
         <div 
           :class="{ answer: true, 'is-answered': isAnswered(qIndex, answer.text) }" 
           v-for="answer in question.answers"
           :key="answer.text"
           @click.prevent = "selectAnswer(answer.is_correct, answer.text, qIndex)"
-        >
-          {{ answer.text }}
-        </div>        
+          v-html="answer.text"
+        />            
       </div>
      </div>
     </transition-group>
@@ -32,7 +31,14 @@ export default {
     },
     isAnswered(index, text) {
       return this.selectedAnswers[`a${index}`].text === text;
+    },
+    questionWithNumber(questionNumber, question) {
+      return `
+      <div class="question-number">
+        <span>${questionNumber + 1}.</span>      
+        <span>${question}</span$>      
+      </div>`;
     }
-  }
+  },
 }
 </script>
