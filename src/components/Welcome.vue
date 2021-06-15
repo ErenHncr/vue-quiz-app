c98bbc8d43e3e51a089ecb5cc3c0858675072419
erenhncr <erenhncr@gmail.com>
2021-06-15 22:07:37 +0300

    Welcome component created

---
+++ src/components/Welcome.vue
@@ -0,0 +1,25 @@
+<template>
+  <p>
+    Take the quiz.
+    <br/>
+    Whenever, you want.
+  </p>
+  <button      
+    type="button"
+    class="btn start-btn"
+    @click.prevent="$emit('start-quiz')"
+  >
+    Start
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Welcome',  
+  emits: ['start-quiz'],
+}
+</script>
+
+<style>
+
+</style>
